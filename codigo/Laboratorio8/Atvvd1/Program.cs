64d20ab406c760fce059f7f9714adcc5770864a5
Souzlipes <78570260+Souzlipes@users.noreply.github.com>
2022-10-27 21:56:23 -0300

    Lab 8

---
+++ codigo/Laboratorio8/Atvvd1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Laboratorio7{
+    class Atvvd1{
+
+        public static void Main(string[] args) {
+             int []vetor = new int[5];
+             int shift = 0;
+
+            for (int j = 0; j < vetor.Length; j++){
+                int valor = j * j;
+                inserirPosicoes(vetor, ref shift, valor);
+            }
+
+            Console.WriteLine(" ");
+            for (int i = 0; i <vetor.Length; i++)
+                Console.Write(" {0} ", vetor[i]);
+            Console.WriteLine(" ");
+            retirarPosicoes(vetor, ref shift);
+            for (int i = 0; i <vetor.Length; i++)
+                Console.Write(" {0} ", vetor[i]);
+        
+            Console.WriteLine("\n"+shift);
+        }
+        public static void inserirPosicoes(int []vetor, ref int shift, int valor){
+            vetor[shift] = valor;
+            shift++;
+        }
+        public static void retirarPosicoes(int []vetor, ref int shift){
+            int aux = vetor[0];
+            for(int i = 0; i < vetor.Length-1; i++){
+                vetor[i] = vetor[i+1];
+            }
+            shift-=1;
+        }
+    }
+}
---
+++ codigo/Laboratorio8/Atvvd2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Laboratorio7{
+    class Atvvd1{
+
+        public static void Main(string[] args) {
+             int []vetor = new int[5];
+             int shift = 0;
+
+            for (int j = 0; j < vetor.Length; j++){
+                int valor = j * j;
+                inserirPosicoes(vetor, ref shift, valor);
+            }
+
+            Console.WriteLine(" ");
+            for (int i = 0; i <vetor.Length; i++)
+                Console.Write(" {0} ", vetor[i]);
+            Console.WriteLine(" ");
+            shift = retirarPosicoes(vetor,ref shift);
+            for (int i = 0; i <vetor.Length; i++)
+                Console.Write(" {0} ", vetor[i]);
+        
+            Console.WriteLine("\n"+shift);
+        }
+        public static void inserirPosicoes(int []vetor, ref int shift, int valor){
+            vetor[shift] = valor;
+            shift++;
+        }
+        public static void retirarPosicoes(int []vetor, ref int shift){
+
+            console.WriteLine("Valor retirado é: {0}",vetor[shift]);
+            shift-=1;
+        }
+    }
+}
