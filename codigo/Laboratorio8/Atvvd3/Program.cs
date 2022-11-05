a2e0d5b4ebe4f0518388d5e9b0410835faa20ffb
Souzlipes <78570260+Souzlipes@users.noreply.github.com>
2022-11-05 01:06:17 -0300

    Lab8
    Finalização

--- codigo/Laboratorio8/Atvvd3/Program.cs
+++ codigo/Laboratorio8/Atvvd3/Program.cs
@@ -9,50 +9,87 @@ namespace Laboratorio7{
              int shift = 0;
              int valor = 0;
 
-            for (int j = 0; j < 5/2; j++){
+            for (int j = 0; j < vetor.Length; j++){
                 valor = j * j;
                 if (shift < 5){
                     Console.WriteLine("Inserindo valor: " + valor);
                     InserirFila(vetor, ref shift, valor);
                     shift += 1;
+                    Console.WriteLine(shift);
                 }
             }
 
-            Console.WriteLine(vetor);
+            for (int i = 0; i < vetor.Length; i++)
+                    Console.Write(" "+vetor[i]);
 
+            Console.WriteLine(" ");
+           
+           string retirar = "";
 
-            valor = 5 * 5;
-            if (shift < 5){
-                InserirFila(vetor, ref shift, valor);
-                shift += 1;
-                Console.WriteLine(vetor);
-            }else{
-                inserirPosicoes(-1, vetor,  ref shift, valor + 2);
-                shift += 1;
-                inserirPosicoes(3, vetor, ref shift, valor);
-                shift += 1;
-                inserirPosicoes(9, vetor, ref shift, valor * valor);
-                shift += 1;
-            }
-        }
-        public static int[] inserirPosicoes(int flag,int []vetor, ref int shift, int valor){
-            if (flag < 0)
-            {
-                vetor = InserirFila(vetor, ref shift, valor);
-                return vetor;
-            }
-            if (flag > shift)
-                flag = shift;
-            for(int i = shift; i > flag; i--)
+            Console.WriteLine("Deseja retirar posições na fila? ");
+            retirar = Console.ReadLine();
+            retirar = retirar.ToUpper();
+            
+           if(retirar == "S"){
+                Console.WriteLine("Qual valor? ");
+                valor = int.Parse(Console.ReadLine());
+
+                retirarPosicoes(vetor, ref shift, valor);
                 
-            {
-                vetor[i] = vetor[i - 1];
-            }
-            vetor[flag] = valor;
+
+                for (int i = 0; i < vetor.Length; i++)
+                    Console.Write(" "+vetor[i]);
+
+                Console.WriteLine(" ");
+
+
+                string inserir = "";
+
+                Console.WriteLine("Deseja reenserir alguma posição?");
+                inserir = Console.ReadLine();
+                inserir = inserir.ToUpper();
+
+                if(inserir == "S"){
+                    Console.WriteLine("Qual valor? ");
+                    valor = int.Parse(Console.ReadLine());
+
+                    inserirPosicoes(vetor, ref shift, valor);
+
+                    for (int i = 0; i < vetor.Length; i++)
+                        Console.Write(" "+vetor[i]);
+                }
+            
+           }
+           if(retirar == "N"){
+            
+           }
+        }
+        public static int[] inserirPosicoes(int []vetor, ref int shift, int valor){
+            vetor[shift] = valor;
+            shift++;
             return vetor;
         }
 
-        public static int[] InserirFila(int[] vetor, ref  int shift, int valor){
+        public static int[] retirarPosicoes(int []vetor, ref int shift, int valor){
+            int aux;
+
+            for(int i = 0; i < vetor.Length; i++){
+                if(valor == vetor[i]){
+                    aux  = vetor[i];
+                    Console.WriteLine(vetor[i]);
+                    vetor[i] = vetor[shift-1];
+                    shift = i;
+                    Console.WriteLine("Valor retirado: {0}",aux);
+                    break;
+                }
+                if(valor != vetor[i]){
+                    Console.WriteLine("Valor não encontrado");
+                }
+            }
+            return vetor;
+        }
+
+        public static int[] InserirFila(int[] vetor, ref  int shift, int valor){  
             vetor[shift] = valor;
             return vetor;
         }
---
+++ codigo/Laboratorio9/Atvvd1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Laboratorio7{
+    class Atvvd1{
+
+        public static void Main(string[] args) {
+             int []vetor = new int[5];
+             int[] vetorFinal = new int[5];
+             int shift = 0;
+
+            for (int j = 0; j < vetor.Length; j++){
+                int valor = j * j;
+                inserirPosicoes(vetor, ref shift, valor);
+            }
+
+            Console.WriteLine(" ");
+            for (int i = 0; i <vetor.Length; i++)
+                Console.Write(" {0} ", vetor[i]);
+            Console.WriteLine(" ");
+            copiaFila(vetor, vetorFinal, ref shift);
+            for (int i = 0; i <vetor.Length; i++)
+                Console.Write(" {0} ", vetor[i]);
+            Console.WriteLine(" ");
+            for (int i = 0; i <vetor.Length; i++)
+                Console.Write(" {0} ", vetorFinal[i]);    
+        
+            Console.WriteLine("\n"+shift);
+        }
+        public static void inserirPosicoes(int []vetor, ref int shift, int valor){
+            vetor[shift] = valor;
+            shift++;
+        }
+        public static void retirarPosicoes(int []vetor, ref int shift, int[] vetorFinal){
+            int fim = shift - 1;
+            Console.WriteLine(fim);
+            for(int i = 1; i < shift; i++){
+                int aux = vetor[i-1];
+                
+                vetor[i-1] = vetor[i];
+                vetorFinal[i-1] = aux;
+                vetorFinal[i]=vetor[fim];
+            }
+            
+            //vetorFinal[fim]=vetor[fim];
+            shift-=1;
+        }
+        public static void copiaFila(int[] vetor, int[] vetorFinal, ref int shift){
+            //for (int i = 0; i < vetor.Length; i++){
+            retirarPosicoes(vetor, ref shift,vetorFinal);
+            //}
+        }
+    }
+}
